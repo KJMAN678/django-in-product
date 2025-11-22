e51c42338e3158666bac1d9d04cc829dcdb6b9bd
KJMAN678 <kj.fireman5@gmail.com>
2025-11-22 20:46:29 +0900

    テスト追加.

--- backend/api/tests/test_api.py
+++ backend/api/tests/test_api.py
@@ -5,8 +5,18 @@ from api.views import HelloWorldView
 
 
 class TestHelloWorldView:
+    def test_get_success(self):
+        factory = APIRequestFactory()
+        view = HelloWorldView.as_view()
+
+        url = "http://127.0.0.1:8000/v1/api/hello-world/"
+        request = factory.get(url)
+
+        response = view(request, version="v1")
+        assert response.status_code == 200
+
     @pytest.mark.parametrize("version", ["v1", "v2"])
-    def test_get(self, version):
+    def test_get_success_version(self, version):
         factory = APIRequestFactory()
         view = HelloWorldView.as_view()
 
@@ -15,3 +25,14 @@ class TestHelloWorldView:
 
         response = view(request, version=version)
         assert response.data == {"message": f"Hello, World! {version}"}
+
+    def test_get_fail_version(self):
+        factory = APIRequestFactory()
+        view = HelloWorldView.as_view()
+        version = "v3"
+
+        url = f"http://127.0.0.1:8000/{version}/api/hello-world/"
+        request = factory.get(url)
+
+        response = view(request, version=version)
+        assert response.data == {"detail": "URLパス内のバージョンが不正です。"}
