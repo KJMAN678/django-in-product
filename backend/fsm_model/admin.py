65b6bcb6ed84de6c33be75f7c9459e5b75289d9c
KJMAN678 <kj.fireman5@gmail.com>
2025-12-06 17:03:15 +0900

    Adminのボタンの日本語化に成功.

--- backend/fsm_model/admin.py
+++ backend/fsm_model/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django_fsm import transition
 from fsm_admin.mixins import FSMTransitionMixin
 
 from fsm_model.models import MonitorPost
--- backend/fsm_model/models.py
+++ backend/fsm_model/models.py
@@ -3,20 +3,48 @@ from django_fsm import FSMField, transition
 
 
 class MonitorPost(models.Model):
-    state = FSMField(default="applying")
+    title = models.CharField(max_length=100)
+    state = FSMField(default="応募中")
 
-    @transition(field=state, source="applying", target="won")
+    def __str__(self):
+        return self.title
+
+    @transition(
+        field=state, source="応募中", target="当選", custom={"button_name": "当選"}
+    )
     def won(self):
-        pass
+        """
+        This function may contain side-effects,
+        like updating caches, notifying users, etc.
+        The return value will be discarded.
+        """
 
-    @transition(field=state, source="applying", target="lost")
+    @transition(
+        field=state, source="応募中", target="落選", custom={"button_name": "落選"}
+    )
     def lost(self):
-        pass
+        """
+        This function may contain side-effects,
+        like updating caches, notifying users, etc.
+        The return value will be discarded.
+        """
 
-    @transition(field=state, source="won", target="lost")
+    @transition(
+        field=state, source="当選", target="不採用", custom={"button_name": "不採用"}
+    )
     def not_selected(self):
-        pass
+        """
+        This function may contain side-effects,
+        like updating caches, notifying users, etc.
+        The return value will be discarded.
+        """
 
-    @transition(field=state, source="lost", target="not_selected")
+    @transition(
+        field=state, source="当選", target="辞退", custom={"button_name": "辞退"}
+    )
     def withdrawn(self):
-        pass
+        """
+        This function may contain side-effects,
+        like updating caches, notifying users, etc.
+        The return value will be discarded.
+        """
